bf35c3653bd01843e62a75acebd92d173258b685
Stephen Watts <stephen.watts@ahpra.gov.au>
2019-04-23 20:03:57 +1000

    Added issuelist header buttons for actions

--- src/IssueList.js
+++ src/IssueList.js
@@ -59,6 +59,38 @@ class IssueList extends Component {
       isAuthorToggleOn: !state.isAuthorToggleOn
     }));
   }
+  handleLabelClick() {
+    this.setState(state => ({
+      isLabelsToggleOn: !state.isLabelsToggleOn
+    }));
+  }
+  handleProjectClick() {
+    this.setState(state => ({
+      isProjectsToggleOn: !state.isProjectsToggleOn
+    }));
+  }
+  handleMilestoneClick() {
+    this.setState(state => ({
+      isMilestonesToggleOn: !state.isMilestonesToggleOn
+    }));
+  }
+  handleAssigneeClick() {
+    this.setState(state => ({
+      isAssigneeToggleOn: !state.isAssigneeToggleOn
+    }));
+  }
+  handleSortClick() {
+    this.setState(state => ({
+      isSortToggleOn: !state.isSortToggleOn
+    }));
+  }
+
+  getAuthorData = (issues) => {
+    return issues.map(issue => {
+      return issue.user
+    })
+  }
+
 
 
   render() {
@@ -67,11 +99,47 @@ class IssueList extends Component {
     const closedIssuesCount = issues.filter(item => item.state === 'closed').length;
 
     let authorButton;
+    let labelsButton;
+    let projectsButton;
+    let milestonesButton;
+    let assigneeButton;
+    let sortButton;
 
     if (this.state.isAuthorToggleOn) {
-      authorButton = <Author action={this.handleAuthorClick} />;
+      let authorData = this.getAuthorData(myissues);
+      authorButton = <Author action={this.handleAuthorClick} title="Filter by author" placeholderText="Filter users" isSearchable="true" data={authorData} filter={this.processFilter}/>;
     } else {
       authorButton = null;
+    }
+
+    if (this.state.isLabelsToggleOn) {
+      labelsButton = <Author action={this.handleLabelClick} title="Filter by label" placeholderText="Filter labels" helpText="hi" isSearchable={true} defaultOption="Unlabeled" />;
+    } else {
+      labelsButton = null;
+    }
+
+    if (this.state.isProjectsToggleOn) {
+      projectsButton = <Author action={this.handleProjectClick} title="Filter by project" placeholderText="Filter projects" isSearchable={true}/>;
+    } else {
+      projectsButton = null;
+    }
+
+    if (this.state.isMilestonesToggleOn) {
+      milestonesButton = <Author action={this.handleMilestoneClick} title="Filter by milestone" placeholderText="Filter milestones" isSearchable={true} defaultOption="Issues with no milestone" />;
+    } else {
+      milestonesButton = null;
+    }
+
+    if (this.state.isAssigneeToggleOn) {
+      assigneeButton = <Author action={this.handleAssigneeClick} title="Filter by who's assigned" placeholderText="Filter users" isSearchable={true} defaultOption="Assigned to nobody" />;
+    } else {
+      assigneeButton = null;
+    }
+
+    if (this.state.isSortToggleOn) {
+      sortButton = <Author action={this.handleSortClick} title="Sort by" isSearchable={false} />;
+    } else {
+      sortButton = null;
     }
 
     return (
@@ -79,32 +147,42 @@ class IssueList extends Component {
         <table>
           <thead>
             <tr>
-            <th>
-            <td>
-            <div className="IssueList-tableHeader">
-              <div className="IssueList-tableHeaderItem">
-                <svg viewBox="0 0 14 16" version="1.1" width="14" height="16"><path d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"></path></svg>
-                {openIssuesCount} Open
+              <th>
+                <div className="IssueList-tableHeader">
+                <div className="IssueList-tableHeaderItem">
+                  <svg viewBox="0 0 14 16" version="1.1" width="14" height="16"><path d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"></path></svg>
+                  {this.state.openIssuesCount} Open
+                </div>
+                <div className="IssueList-tableHeaderItem">
+                  <svg viewBox="0 0 12 16" version="1.1" width="12" height="16" ><path d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"></path></svg>
+                  {this.state.closedIssuesCount} Closed 
+                </div>
+                <button onClick={this.handleAuthorClick} className="IssueList-tableHeaderButtonArrowStart">
+                    Author <div className="IssueList-tableHeaderButtonArrow" />
+                </button>
+                {authorButton}
+                <button onClick={this.handleLabelClick}>
+                    Labels <div className="IssueList-tableHeaderButtonArrow" />
+                    {labelsButton}
+                </button>
+                <button onClick={this.handleProjectClick}>
+                    Projects <div className="IssueList-tableHeaderButtonArrow" />
+                    {projectsButton}
+                </button>
+                <button onClick={this.handleMilestoneClick}>
+                    Milestones <div className="IssueList-tableHeaderButtonArrow" />
+                    {milestonesButton}
+                </button>
+                <button onClick={this.handleAssigneeClick}>
+                    Assignee <div className="IssueList-tableHeaderButtonArrow" />
+                    {assigneeButton}
+                </button>
+                <button onClick={this.handleSortClick}>
+                    Sort <div className="IssueList-tableHeaderButtonArrow" />
+                    {sortButton}
+                </button>
               </div>
-              <div className="IssueList-tableHeaderItem">
-                <svg viewBox="0 0 12 16" version="1.1" width="12" height="16" ><path d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"></path></svg>
-                {closedIssuesCount} Closed 
-              </div>
-              <button onClick={this.handleAuthorClick}>
-                  Author <div className="IssueList-tableHeaderButtonArrow" />
-                  {authorButton}
-              </button>
-
-
-              
-              Labels
-              Projects
-              Milestones
-              Assignee
-              Sort
-            </div>
-            </td>
-            </th>
+              </th>
             </tr>
           </thead>
           <tbody>
