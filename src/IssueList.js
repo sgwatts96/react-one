8c0301fb4c039b4682ff37187e6caee383264afd
Stephen Watts <stephen.watts@ahpra.gov.au>
2019-04-25 10:35:21 +1000

    Remove <table> and replace with flexbox
    Updated the css to use flexbox to handler the table header and adjusted
    buttons according to new container

--- src/IssueList.js
+++ src/IssueList.js
@@ -47,15 +47,12 @@ class IssueList extends Component {
     return;
   }
 
-  getHeaderButton = (buttonLabel, toggleVariable, isStart) => {
-    let buttonClassName = (isStart ? 'IssueList-tableHeaderButtonArrowStart': '')
-    
-    let button =  <button className={buttonClassName} onClick={() => this.setState({ [toggleVariable]: !this.state[toggleVariable] })}>
-                              {buttonLabel} <div className="IssueList-tableHeaderButtonArrow" />
+  getHeaderButton = (buttonLabel, toggleVariable) => {    
+    let button =  <button onClick={() => this.setState({ [toggleVariable]: !this.state[toggleVariable] })}>
+                    {buttonLabel} <div className="IssueList-headerItemArrow" />
                   </button>
     return button
   }
-
 
   render() {
     let authorData = this.getAuthorData(this.state.issues);
@@ -120,38 +117,25 @@ class IssueList extends Component {
                       </Popover>
 
     return (
-      <div className='container1'>
-        <table>
-          <thead>
-            <tr>
-              <th>
-                <div className="IssueList-tableHeader">
-                <div className="IssueList-tableHeaderItem">
-                  <svg viewBox="0 0 14 16" version="1.1" width="14" height="16"><path d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"></path></svg>
-                    {this.state.issues.filter(item => item.state === 'open').length} Open
-                </div>
-                <div className="IssueList-tableHeaderItem">
-                  <svg viewBox="0 0 12 16" version="1.1" width="12" height="16" ><path d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"></path></svg>
-                    {this.state.issues.filter(item => item.state === 'closed').length} Closed 
-                </div>
-                {authorButton}
-                {labelsButton}
-                {projectsButton}
-                {milestonesButton}
-                {assigneeButton}
-                {sortButton}
-              </div>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                {this.createIssues(this.state.issues)}
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div className='IssueList-mainContainer'>
+        <div className="IssueList-header">
+          <button className="IssueList-headerItem">
+            <svg viewBox="0 0 14 16" version="1.1" width="14" height="16"><path d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"></path></svg>
+              {this.state.issues.filter(item => item.state === 'open').length} Open
+          </button>
+          <button className="IssueList-headerItem">
+            <svg viewBox="0 0 12 16" version="1.1" width="12" height="16" ><path d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"></path></svg>
+              {this.state.issues.filter(item => item.state === 'closed').length} Closed 
+          </button>
+          <div className="IssueList-headerItemSpacer" />
+          {authorButton}
+          {labelsButton}
+          {projectsButton}
+          {milestonesButton}
+          {assigneeButton}
+          {sortButton}
+        </div>
+        {this.createIssues(this.state.issues)}
       </div>
     )
   }
