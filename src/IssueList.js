72ffaa0e99841db140a60dc5ca9b26c530058d6f
Stephen Watts <stephen.watts@ahpra.gov.au>
2019-05-02 14:32:56 +1000

    Point Open Closed button to filter Method

--- src/IssueList.js
+++ src/IssueList.js
@@ -172,13 +172,13 @@ class IssueList extends Component {
     return (
       <div className='IssueList-mainContainer'>
         <div className="IssueList-header">
-          <button className="IssueList-headerItem">
+          <button className="IssueList-headerItem" onClick={() => this.processFilter({type: 'Open/Closed', criteria : 'open'})}>
             <svg viewBox="0 0 14 16" version="1.1" width="14" height="16"><path d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"></path></svg>
-              {this.state.issues.filter(item => item.state === 'open').length} Open
+              {this.state.originalIssues.filter(item => item.state === 'open').length} Open
           </button>
-          <button className="IssueList-headerItem">
+          <button className="IssueList-headerItem" onClick={() => this.processFilter({type: 'Open/Closed', criteria : 'closed'})}>
             <svg viewBox="0 0 12 16" version="1.1" width="12" height="16" ><path d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"></path></svg>
-              {this.state.issues.filter(item => item.state === 'closed').length} Closed 
+              {this.state.originalIssues.filter(item => item.state === 'closed').length} Closed 
           </button>
           <div className="IssueList-headerItemSpacer" />
           {authorButton}
